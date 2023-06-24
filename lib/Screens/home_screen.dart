e63bd8f08452b5312cf251504d5006518b86c6fa
Amin-Thoufeeque <aminthoufeeque10@gmail.com>
2023-06-24 18:09:32 +0530

    Create button added

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:biodata_app/Screens/biodata_screen.dart';
 import 'package:flutter/material.dart';
 
 import '../Widgets/biodata_widget.dart';
@@ -16,13 +17,51 @@ class HomeScreen extends StatelessWidget {
             style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
           ),
         ),
-        body: GridView.builder(
-          gridDelegate:
-              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-          itemCount: 3,
-          itemBuilder: (BuildContext context, int index) {
-            return const BiodataWidget();
-          },
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 50,
+                width: 350,
+                decoration: BoxDecoration(
+                    color: Colors.yellow,
+                    borderRadius: BorderRadius.circular(10)),
+                child: TextButton.icon(
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) {
+                          return const BioDataScreen();
+                        },
+                      ));
+                    },
+                    icon: const Icon(
+                      Icons.add,
+                      color: Colors.black,
+                      size: 35,
+                    ),
+                    label: const Text(
+                      'Add Biodata',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 20),
+                    )),
+              ),
+            ),
+            const SizedBox(
+              height: 1,
+            ),
+            GridView.builder(
+              gridDelegate:
+                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+              itemCount: 3,
+              shrinkWrap: true,
+              itemBuilder: (BuildContext context, int index) {
+                return const BiodataWidget();
+              },
+            ),
+          ],
         ));
   }
 }
